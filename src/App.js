9c0fe4592eeccebdfa2b7902c449003bba8f4e7e
ram <ram.tripathi@neosoftmail.com>
2020-09-08 10:37:06 +0530

    Commiting react client-code

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Dashboard from "/Components/Dashboard/Dashboard";
+import Broadcasting from "./Components/Broadcasting/Broadcasting";
+import Publicroom from "/Components/Publicroom/Publicroom";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/broadcasting" component={Broadcasting} />
+          <Route exact path="/public-room" component={Publicroom} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+export default App;
